8655915b3c903ca63d57dfbf4391817caa27ad2c
flyosprey <70664311+flyosprey@users.noreply.github.com>
2021-09-30 16:52:15 +0300

    Updated handler.py and serverless.yml

--- handler.py
+++ handler.py
@@ -1,21 +1,36 @@
 import json
+import uuid
+import boto3
+import logging
+from botocore.exceptions import ClientError
+from .constants import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, DEFAULT_REGION_NAME
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
-    }
+def create_presigned_url(event, expiration=5*60):
+    s3_client = boto3.client("s3", region_name=DEFAULT_REGION_NAME,
+                             aws_access_key_id=AWS_ACCESS_KEY_ID,
+                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    try:
+        url = s3_client.generate_presigned_url("put_object",
+                                               Params={"Bucket": event["Bucket_name"],
+                                                       "Key": event["Bucket_key"]
+                                                       },
+                                               HttpMethod="PUT",
+                                               ExpiresIn=expiration)
+
+    except ClientError as e:
+        logging.error(e)
+        return None
+
+    return url
+
+
+def create_blob(event, context):
+
+    url = create_presigned_url(event)
+
+    response = {"statusCode": 200, "body": json.dumps(url)}
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
