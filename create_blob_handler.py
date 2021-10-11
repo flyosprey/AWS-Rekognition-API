c901b58efea3a322360e93bc71d411a85723966f
flyosprey <70664311+flyosprey@users.noreply.github.com>
2021-10-11 13:49:42 +0300

    Structured code

--- create_blob_handler.py
+++ create_blob_handler.py
@@ -7,18 +7,13 @@ from botocore.config import Config
 from constants import DEFAULT_REGION_NAME, BUCKET_NAME
 
 
-def create_presigned_url(bucket_key, expiration=5*60):
-    s3_client = boto3.client("s3", config=Config(signature_version="s3v4"),
-                             region_name=DEFAULT_REGION_NAME)
-
+def create_presigned_url(bucket_key):
+    s3_client = boto3.client("s3", config=Config(signature_version="s3v4"), region_name=DEFAULT_REGION_NAME)
     try:
         url = s3_client.generate_presigned_url("put_object",
-                                               Params={"Bucket": BUCKET_NAME,
-                                                       "Key": bucket_key
-                                                       },
+                                               Params={"Bucket": BUCKET_NAME, "Key": bucket_key},
                                                HttpMethod="PUT",
-                                               ExpiresIn=expiration)
-
+                                               ExpiresIn=3600)
     except ClientError as e:
         logging.error(e)
         return None
@@ -26,26 +21,20 @@ def create_presigned_url(bucket_key, expiration=5*60):
     return url
 
 
-def put_item_dynamodb(callback_url):
-    blob_id = str(uuid.uuid4())
-
+def put_item_dynamodb(callback_url, blob_id):
     client = boto3.resource("dynamodb", region_name=DEFAULT_REGION_NAME)
     table = client.Table("Blobs")
-
     items = {"blob_id": blob_id, "callback_url": callback_url}
-
     response = table.put_item(Item=items)
 
     return response
 
 
 def create_blob(event, context):
-
-    callback_url = event["Callback_url"]
-    url = create_presigned_url(event["Bucket_key"])
-
-    db_responce = put_item_dynamodb(callback_url)
-
-    response = {"statusCode": 200, "body": json.dumps(db_responce)}
+    blob_id = str(uuid.uuid4())
+    callback_url = event["callback_url"]
+    url = create_presigned_url(event["bucket_key"])
+    db_response = put_item_dynamodb(callback_url, blob_id)
+    response = {"status_code": 200, "url": json.dumps(db_response), "blob_id": json.dumps(blob_id)}
 
     return response
