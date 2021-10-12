920bdb9941b31af05d90b3b7037d80560190f84b
flyosprey <70664311+flyosprey@users.noreply.github.com>
2021-10-12 13:41:58 +0300

    Updated

--- create_blob_handler.py
+++ create_blob_handler.py
@@ -1,40 +1,61 @@
 import json
 import uuid
 import boto3
-import logging
+import urllib.request
+import urllib.error
 from botocore.exceptions import ClientError
 from botocore.config import Config
 from constants import DEFAULT_REGION_NAME, BUCKET_NAME
 
 
-def create_presigned_url(bucket_key):
+def create_presigned_url(blob_key):
     s3_client = boto3.client("s3", config=Config(signature_version="s3v4"), region_name=DEFAULT_REGION_NAME)
     try:
-        url = s3_client.generate_presigned_url("put_object",
-                                               Params={"Bucket": BUCKET_NAME, "Key": bucket_key},
-                                               HttpMethod="PUT",
-                                               ExpiresIn=3600)
+        url = s3_client.generate_presigned_url(
+            "put_object",
+            Params={"Bucket": BUCKET_NAME, "Key": blob_key},
+            HttpMethod="PUT",
+            ExpiresIn=3600
+        )
     except ClientError as e:
-        logging.error(e)
-        return None
-
-    return url
+        print(e.response["Error"]["Message"])
+    else:
+        return url
 
 
 def put_item_dynamodb(callback_url, blob_id):
-    client = boto3.resource("dynamodb", region_name=DEFAULT_REGION_NAME)
-    table = client.Table("Blobs")
-    items = {"blob_id": blob_id, "callback_url": callback_url}
-    response = table.put_item(Item=items)
+    try:
+        client = boto3.resource("dynamodb", region_name=DEFAULT_REGION_NAME)
+        table = client.Table("Blobs")
+        items = {"blob_id": blob_id, "callback_url": callback_url}
+        table.put_item(Item=items)
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
 
-    return response
+
+def is_url(url):
+    try:
+        urllib.request.urlopen(url)
+    except urllib.error.URLError or urllib.error.HTTPError:
+        return False
+    else:
+        return True
 
 
 def create_blob(event, context):
     blob_id = str(uuid.uuid4())
+    bucket_key = blob_id + ".png"
     callback_url = event["callback_url"]
-    url = create_presigned_url(event["bucket_key"])
-    db_response = put_item_dynamodb(callback_url, blob_id)
-    response = {"status_code": 200, "url": json.dumps(db_response), "blob_id": json.dumps(blob_id)}
+    if is_url(event["callback_url"]):
+        url = create_presigned_url(bucket_key)
+        put_item_dynamodb(callback_url, blob_id)
+        response = {
+            "status_code": 200,
+            "url": json.dumps(url),
+            "blob_id": json.dumps(blob_id),
+            "callback_url": json.dumps(callback_url)
+        }
+    else:
+        return {"status_code": 400, "message": "Invalid callback url supplied"}
 
     return response
--- process_blob_handler.py
+++ process_blob_handler.py
@@ -1,22 +1,52 @@
 import boto3
 import urllib.parse
 import json
+from decimal import Decimal
+from botocore.exceptions import ClientError
 from constants import BUCKET_NAME, DEFAULT_REGION_NAME
 
 
-def detect_labels(photo):
-    rekognition_client = boto3.client("rekognition", region_name=DEFAULT_REGION_NAME)
-    response = rekognition_client.detect_labels(
-                                                Image={"S3Object": {"Bucket": BUCKET_NAME, "Name": photo}},
-                                                MaxLabels=18
-                                                )
+def detect_photo_labels(s3_blob_key):
+    try:
+        rekognition_client = boto3.client("rekognition", region_name=DEFAULT_REGION_NAME)
+        labels = rekognition_client.detect_labels(
+            Image={"S3Object": {"Bucket": BUCKET_NAME, "Name": s3_blob_key}},
+            MaxLabels=10
+        )
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+    else:
+        for label in labels["Labels"]:
+            del label["Instances"]
+        labels = json.loads(json.dumps(labels["Labels"]), parse_float=Decimal)
 
-    return response
+        return labels
+
+
+def put_labels_info(labels, dynamodb_blob_id):
+    dynamodb_client = boto3.resource("dynamodb", region_name=DEFAULT_REGION_NAME)
+    table = dynamodb_client.Table("Blobs")
+    try:
+        dynamodb_response = table.update_item(
+            Key={
+                'blob_id': dynamodb_blob_id,
+            },
+            UpdateExpression="set labels_photo=:l",
+            ExpressionAttributeValues={
+                ':l': labels
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+    else:
+        return dynamodb_response
 
 
 def process_blob(event, context):
-    s3 = boto3.client("s3", region_name=DEFAULT_REGION_NAME)
-    file_name = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"], encoding="utf-8")
-    file_object = s3.get_object(Bucket=BUCKET_NAME, Key=file_name)
+    s3_blob_key = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"], encoding="utf-8")
+    dynamodb_blob_id = s3_blob_key.replace(".png", "")
+    labels = detect_photo_labels(s3_blob_key)
+    response = put_labels_info(labels, dynamodb_blob_id)
 
-    return file_object["ContentType"]
+    return response
