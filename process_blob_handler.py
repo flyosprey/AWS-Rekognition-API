10fe35e38ebda9f7edce40f71c646af62c148b42
Vladyslav_Bashtannyk <70664311+flyosprey@users.noreply.github.com>
2021-10-19 13:37:27 +0300

    Update process_blob_handler.py

--- process_blob_handler.py
+++ process_blob_handler.py
@@ -1,5 +1,6 @@
 import boto3
 import urllib.parse
+import urllib3
 import json
 from decimal import Decimal
 from botocore.exceptions import ClientError
@@ -16,14 +17,11 @@ def detect_photo_labels(s3_blob_key):
     except ClientError as e:
         print(e.response["Error"]["Message"])
     else:
-        for label in labels["Labels"]:
-            del label["Instances"]
-        labels = json.loads(json.dumps(labels["Labels"]), parse_float=Decimal)
-
         return labels
 
 
 def put_labels_info(labels, dynamodb_blob_id):
+    labels = json.loads(json.dumps(labels["Labels"]), parse_float=Decimal)
     dynamodb_client = boto3.resource("dynamodb", region_name=REGION_NAME)
     table = dynamodb_client.Table(TABLE_NAME)
     try:
@@ -41,11 +39,35 @@ def put_labels_info(labels, dynamodb_blob_id):
         print(e.response["Error"]["Message"])
     else:
         return dynamodb_response
+        
+def get_callback_url(blob_id):
+    dynamodb_client = boto3.resource("dynamodb", region_name=REGION_NAME)
+    table = dynamodb_client.Table(TABLE_NAME)
+    try:
+        items = table.get_item(Key={"blob_id": blob_id})
+        callback_url = items["Item"]["callback_url"]
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+    else:
+        return callback_url
+        
+def send_callback(callback_url, labels):
+    labels = json.loads(json.dumps(labels["Labels"], default=float))
+    http = urllib3.PoolManager()
+    callback_response = http.request(
+            "POST",
+            callback_url,
+            body=json.dumps(labels),
+            headers={"Content-Type": "application/json"})
+    
+    return callback_response
 
 
 def process_blob(event, context):
     blob_id = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"], encoding="utf-8")
+    callback_url = get_callback_url(blob_id)
     labels = detect_photo_labels(blob_id)
-    response = put_labels_info(labels, blob_id)
+    send_callback_response = send_callback(callback_url, labels)
+    put_labels_info_response = put_labels_info(labels, blob_id)
 
-    return response
+    return put_labels_info_response
