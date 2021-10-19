b8f64b1ec46365219ec5c005e1fc5201490a4f5b
Vladyslav_Bashtannyk <70664311+flyosprey@users.noreply.github.com>
2021-10-19 13:36:45 +0300

    Update make_callback_handler.py

--- make_callback_handler.py
+++ make_callback_handler.py
@@ -8,6 +8,7 @@ from constants import REGION_NAME, TABLE_NAME, REGION_NAME
 def get_labels_info(blob_id):
     dynamodb_resource = boto3.resource("dynamodb", region_name=REGION_NAME)
     table = dynamodb_resource.Table(TABLE_NAME)
+
     try:
         response = table.get_item(Key={"blob_id": blob_id})
     except ClientError as e:
@@ -17,24 +18,14 @@ def get_labels_info(blob_id):
 
 
 def make_callback(event, context):
-    http = urllib3.PoolManager()
     blob_id = event["pathParameters"]["blob_id"]
     dynamodb_response = get_labels_info(blob_id)
     if "Item" not in dynamodb_response.keys():
         error_404 = {"message": "Blob not found"}
         return {"statusCode": 404, "body": json.dumps(error_404)}
     else:
-        callback_url = dynamodb_response["Item"]["callback_url"]
-        labels_info = json.dumps(dynamodb_response["Item"]["labels_photo"], ensure_ascii=False, default=str)
-        callback_response = http.request(
-            "POST",
-            callback_url,
-            body=labels_info,
-            headers={"Content-Type": "application/json"}
-        )
-    if callback_response.status != 200:
-        error = {"message": "Problem with your callback url"}
-        return {"statusCode": callback_response.status, "body": json.dumps(error)}
-    else:
-        response = {"callback_url": callback_url, "labels": labels_info}
+        labels_info = json.loads(json.dumps(dynamodb_response["Item"]["labels_photo"], default=float))
+        print(type(labels_info))
+        print(labels_info[0]["Confidence"])
+        response = {"labels": labels_info}
         return {"statusCode": 200, "body": json.dumps(response)}
